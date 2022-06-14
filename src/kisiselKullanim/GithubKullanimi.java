0a05c47eadd8ea65da615d191c2294ecebebbf01
Omer <mrfkrtl89@gmail.com>
2022-06-14 14:00:24 +0300

    2. degisiklik

--- src/kisiselKullanim/GithubKullanimi.java
+++ src/kisiselKullanim/GithubKullanimi.java
@@ -4,5 +4,11 @@ public class GithubKullanimi {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        System.out.println("that's it");
+
+
+        System.out.println("Omer branchinda ortak class'da degisiklik yaptim");
+
     }
 }
