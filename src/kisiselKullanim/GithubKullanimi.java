64199af910ef1e1ec07161119c02cd40f83a12cf
Omer <mrfkrtl89@gmail.com>
2022-06-14 14:13:45 +0300

    main  branch degisiklik

--- src/kisiselKullanim/GithubKullanimi.java
+++ src/kisiselKullanim/GithubKullanimi.java
@@ -4,5 +4,12 @@ public class GithubKullanimi {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+
+        System.out.println("main branch degisiklik 1.satir ");
+        System.out.println("main branch degisiklik 2.satir ");
+
+
+
     }
 }
